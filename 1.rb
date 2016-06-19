9bb295d3f47fd9f1db3c797e8e60015d2ab587e7
Fikrat Talibli <fikrattalibli@gmail.com>
2016-06-19 18:41:26 +0500

    3rd version-Flow control Week 2

--- 1.rb
+++ 1.rb
@@ -12,3 +12,21 @@ until a>=15
 	puts a
 	a+=1
 end
+
+a=9
+
+a=5
+b=0
+
+puts "One liner" if a==5 and b==0 #=>One liner
+c="coursera"
+puts "coursera" if Integer===21 
+case c
+	when "coursera" then puts "This string is coursera"
+	when "Smith" then puts "Your name is Smith"
+end
+puts '------'
+puts ''
+for i in 0..10
+	puts i
+end
